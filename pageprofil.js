2bb6e5d3c2c69587563263a3260f435ba6586349
Aymeric Choquet <aymeric.choquet@gmail.com>
2023-10-19 16:37:08 +0200

    maj

--- pageprofil.js
+++ pageprofil.js
@@ -1,9 +1,10 @@
 const addBadgeButton = document.getElementById('add-badge-button');
-const badgesContainer = document.getElementById('badges');
+const badgesContainer = document.querySelector('.badges');
 
 addBadgeButton.addEventListener('click', (e) => {
     e.preventDefault();
     const badgeName = prompt('Nom du badge :');
+    console.log(badgeName);
     if (badgeName) {
         const newBadge = document.createElement('div');
         newBadge.className = 'badge';

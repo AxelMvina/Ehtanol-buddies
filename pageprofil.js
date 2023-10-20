efa1bc8279121c74388ef00e1a7f941f5378df23
Aymeric Choquet <aymeric.choquet@gmail.com>
2023-10-20 10:31:21 +0200

    maj

--- pageprofil.js
+++ pageprofil.js
@@ -13,3 +13,33 @@ addBadgeButton.addEventListener('click', (e) => {
         badgesContainer.appendChild(newBadge);
     }
 });
+
+const openModalButton = document.getElementById('open-modal');
+const modal = document.getElementById('myModal');
+const closeModalButton = document.getElementById('close-modal');
+const addCategoryButton = document.getElementById('add-category-button');
+const categoryNameInput = document.getElementById('category-name');
+
+openModalButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+addCategoryButton.addEventListener('click', () => {
+    const categoryName = categoryNameInput.value;
+    if (categoryName) {
+        const newCategory = document.createElement('div');
+        newCategory.className = 'category';
+        newCategory.textContent = categoryName;
+        modal.style.display = 'none';
+    }
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});

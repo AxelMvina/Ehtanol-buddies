b2e286308c7a824a0ee0cd31015a73000ac20966
axel mandon <mandon.axel25@gmail.com>
2023-10-25 15:44:09 +0200

    maj

--- test.js
+++ test.js
@@ -1,33 +1,60 @@
-const evenements = [
-    {
-      titre: "Chez JP",
-      descriptif: "Soirée cool chez JP",
-      gensRecherches: ["Sam", "l'ambianceur", "Cuistot"],
-      gensInvites: ["John Doe", "Jane Smith"],
-      date: "2023-11-20",
-      lieu: "Montcul",
-    },
-    {
-      titre: "Bubulle",
-      descriptif: "Soirée bulle maillot requis",
-      gensRecherches: ["Sam", "Le gros buveur"],
-      gensInvites: ["Alice Johnson", "Kikou du 62", "Charlie Br"],
-      date: "2023-11-05",
-      lieu: "Chez tonton, bar Montcul",
-    },
-    {
-      titre: "100 ans de Gégé",
-      descriptif: "Soirée chic mais pas sans alcool",
-      gensRecherches: ["Sam", "Cuistot"],
-      gensInvites: ["Eva Ada", "David Mill"],
-      date: "2023-11-15",
-      lieu: "CH Roubaix, Gériartrie",
-    },
-  ];
-  
-  console.log("Titre de l'événement : " + evenements[0].titre);
-  console.log("Descriptif : " + evenements[0].descriptif);
-  console.log("Gens recherchés : " + evenements[0].gensRecherches.join(", "));
-  console.log("Gens déjà invités : " + evenements[0].gensInvites.join(", "));
-  console.log("Date : " + evenements[0].date);
-  console.log("Lieu : " + evenements[0].lieu);
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const openModalButton = document.getElementById('openModal');
+    const modal = document.getElementById('myModal');
+    const closeButton = document.querySelector('.close');
+    const friendList = document.getElementById('friendList');
+
+    const chatModal = document.getElementById('chatModal');
+    const closeChatButton = document.querySelector('.close-chat');
+    const chatFriendName = document.getElementById('chatFriendName');
+    const chatMessages = document.getElementById('chatMessages');
+    const messageInput = document.getElementById('messageInput');
+    const sendMessageButton = document.getElementById('sendMessage');
+
+    const friends = ['Ami 1', 'Ami 2', 'Ami 3', 'Ami 4'];
+
+    openModalButton.addEventListener('click', function() {
+        modal.style.display = 'block';
+        friends.forEach(friend => {
+            const listItem = document.createElement('li');
+            listItem.textContent = friend;
+            friendList.appendChild(listItem);
+        });
+    });
+
+    closeButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+        friendList.innerHTML = '';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+            friendList.innerHTML = '';
+        }
+    });
+
+    friendList.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target.tagName === 'LI') {
+            const friendName = target.textContent;
+            chatFriendName.textContent = friendName;
+            chatModal.style.display = 'block';
+        }
+    });
+
+    closeChatButton.addEventListener('click', function() {
+        chatModal.style.display = 'none';
+    });
+
+    sendMessageButton.addEventListener('click', function() {
+        const message = messageInput.value;
+        if (message.trim() !== '') {
+            const messageElement = document.createElement('div');
+            messageElement.textContent = message;
+            chatMessages.appendChild(messageElement);
+            messageInput.value = '';
+        }
+    });
+});
